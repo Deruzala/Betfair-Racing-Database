ff941f67d07695159bfeb360112d0ff5b1dc3d8f
Deruzala <Deruzala@Local>
2021-05-21 13:04:00 +0100

    added-distance-conversion

--- HorseRacing/dbo/Views/RaceCard-view.sql
+++ HorseRacing/dbo/Views/RaceCard-view.sql
@@ -5,8 +5,12 @@ SELECT
     rc.RaceDate, 
     c.Name AS course,
     h.Name AS horse,
-    FORMAT(dbo.fn_GetAvgTickDrop(rc.horseID, rc.RaceDate),'0.00') AS AvgTickDrop
+    FORMAT(dbo.fn_GetAvgTickDrop(rc.horseID, rc.RaceDate),'0.00') AS AvgTickDrop,
+    distance.Name AS Distance,
+    distance.Meters As DistanceM,
+    distance.Furlongs AS DistanceF
 
 FROM RaceCard rc 
 LEFT JOIN course c ON c.ID = rc.courseID 
 LEFT JOIN horse h ON h.ID = rc.horseID
+LEFT JOIN distance ON distance.ID = rc.DistanceID
