c5ecfad42e43ad353df104f3ed4bfe35b363d970
Deruzala <55004185+Deruzala@users.noreply.github.com>
2021-05-21 22:31:44 +0100

    updated-view

--- HorseRacing/dbo/Views/Records-Join.sql
+++ HorseRacing/dbo/Views/Records-Join.sql
@@ -1,4 +1,4 @@
-﻿CREATE VIEW [dbo].[Records-Join]
+CREATE VIEW [dbo].[Records-Join]
 AS 
 
 SELECT 
@@ -7,7 +7,9 @@ SELECT
     c.Name AS course,
     h.Name AS horse,
     s.Name AS system,
+    m.Name AS marketType,
     b.Name AS bid,
+    o.Name AS outcome,
     r.pnl
 
 FROM RaceCard rc 
@@ -17,4 +19,6 @@ ON rc.ID=r.ID
 LEFT JOIN course c ON c.ID = rc.courseID 
 LEFT JOIN horse h ON h.ID = rc.horseID
 LEFT JOIN system s ON s.ID = r.systemID
+LEFT JOIN marketType m ON m.ID = r.MarketId
 LEFT JOIN bid b ON b.ID = r.bidID
+LEFT JOIN outcome o ON o.ID = r.outcomeID
